3adb096a3783f742e4c43d5a8f54947a4474605f
Yuri Moreira <75076180+Yurimrrr@users.noreply.github.com>
2022-08-31 15:41:34 -0300

    Desafio finalizado

--- Models/Iphone.cs
+++ Models/Iphone.cs
@@ -1,8 +1,15 @@
 namespace DesafioPOO.Models
 {
-    // TODO: Herdar da classe "Smartphone"
-    public class Iphone
+  // TODO: Herdar da classe "Smartphone"
+  public class Iphone : Smartphone
+  {
+    public Iphone(string numero, string modelo, string _IMEI, int memoria) : base(numero, modelo, _IMEI, memoria)
     {
-        // TODO: Sobrescrever o método "InstalarAplicativo"
     }
+
+    public override void InstalarAplicativo(string nomeApp)
+    {
+      Console.WriteLine("Instalando " + nomeApp + " no Iphone!");
+    }
+  }
 }
--- Program.cs
+++ Program.cs
@@ -1,3 +1,15 @@
 ﻿using DesafioPOO.Models;
 
-// TODO: Realizar os testes com as classes Nokia e Iphone
+Console.WriteLine("Smartphone Nokia");
+
+Smartphone nokia = new Nokia("123456789", "Modelo1", "1111111111", 64);
+nokia.Ligar();
+nokia.InstalarAplicativo("Zap");
+
+Console.WriteLine("\n");
+
+Console.WriteLine("Smartphone Iphone");
+
+Smartphone iphone = new Iphone("122332432", "Modelo2", "1111132331", 128);
+iphone.Ligar();
+iphone.InstalarAplicativo("Zap");
